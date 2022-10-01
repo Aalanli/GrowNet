cf86442fd712be260b3922fcf7c2ae3584678aec
Allan Lin <allanlin666666@gmail.com>
2022-09-30 23:26:41 -0400

    Better Index abstraction

--- grownet-src/src/tensor/index.rs
+++ grownet-src/src/tensor/index.rs
@@ -1,222 +1,244 @@
 use std::ops::Deref;
+use std::marker::PhantomData;
 
-pub trait TIndex {
-    type MP: TIndex;
-    fn tile_cartesian(&self, dims: &[usize], strides: &[usize], len: usize) -> Option<usize>;
-    fn offset(&self, offsets: &[usize], strides: &[usize]) -> Self::MP;
-}
+// This macro automatically makes
+// unit polymorphic structs with deref auto derived
+macro_rules! auto_deref {
+    ($st_nm:ident) => {
+        pub struct $st_nm<T>(T);
 
-pub struct UnsafeIndex<T>(T);
-
-impl<T> Deref for UnsafeIndex<T> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-fn check_index(index: usize, len: usize) -> Option<usize> {
-    if index > len {
-        return None;
-    }
-    Some(index)
-}
-
-fn tile_cartesian(indices: &[usize], dims: &[usize], strides: &[usize], _len: usize) -> Option<usize> {
-    let mut idx: usize = 0;
-    let offset = dims.len() - indices.len();
-    for i in 0..indices.len() {
-        let i = i + offset;
-        if indices[i] >= dims[i] {
-            return None;
-        }
-        idx += strides[i] * indices[i];
-    }
-    Some(idx)
-}
-
-fn in_bounds_tile_cartesian(indices: &[usize], dims: &[usize], strides: &[usize], _len: usize) -> Option<usize> {
-    let mut idx: usize = 0;
-    let offset = dims.len() - indices.len();
-    for i in 0..indices.len() {
-        let i = i + offset;
-        idx += strides[i] * indices[i];
-    }
-    Some(idx)
-}
-
-fn static_tile_cartesian<const N: usize>(indices: &[usize; N], dims: &[usize], strides: &[usize], _len: usize) -> Option<usize> {
-    let mut idx: usize = 0;
-    let offset = dims.len() - N;
-    for i in 0..N {
-        let i = i + offset;
-        if indices[i] >= dims[i] {
-            return None;
-        }
-        idx += strides[i] * indices[i];
-    }
-    Some(idx)
-}
-
-fn in_bounds_static_tile_cartesian<const N: usize>(indices: &[usize; N], dims: &[usize], strides: &[usize], _len: usize) -> Option<usize> {
-    let mut idx: usize = 0;
-    let offset = dims.len() - N;
-    for i in 0..N {
-        let i = i + offset;
-        idx += strides[i] * indices[i];
-    }
-    Some(idx)
-}
-
-fn offset_ind(indices: &[usize], offsets: &[usize], _strides: &[usize]) -> Vec<usize> {
-    let mut new_indices: Vec<usize> = indices.to_vec();
-    for i in 0..offsets.len() {
-        new_indices[i] += offsets[i];
-    }
-    new_indices
-}
-
-fn static_offset_ind<const N: usize>(indices: &[usize; N], offsets: &[usize], _strides: &[usize]) -> [usize; N] {
-    let mut new_indices = indices.clone();
-    for i in 0..offsets.len() {
-        new_indices[i] += offsets[i];
-    }
-    new_indices
-}
-
-fn inbounds_retile_cart(index: usize, s_strides: &[usize], g_strides: &[usize], _len: usize) -> Option<usize> {
-    let mut idx = 0;
-    let mut ind = index;
-    for i in 0..s_strides.len() {
-        let c = ind / s_strides[i] as usize;
-        idx += c * g_strides[i];
-        ind -= c * s_strides[i];
-        if ind <= 0 {
-            break;
-        }
-    }
-    Some(idx)
-}
-
-fn retile_cart(index: usize, s_strides: &[usize], g_strides: &[usize], len: usize) -> Option<usize> {
-    if let Some(idx) = inbounds_retile_cart(index, s_strides, g_strides, len) {
-        if idx > len {
-            return None;
-        }
-        Some(idx);
-    }
-    None
-}
-
-macro_rules! RepeatFn {
-    ($op_fn:ident, $fn_name:ident($($arg_nm:ident : $tp:ty),*) -> $rt:ty) => {
-        fn $fn_name(&self, $($arg_nm : $tp),*) -> $rt {
-            ($op_fn)(self, $($arg_nm),*)
-        }
-    }
-}
-
-macro_rules! deriveTIndex {
-    ($tp:ty, [$([$op_fn:ident, $fn_name:ident($($arg_nm:ident : $atp:ty),*) -> $rt:ty]),*]) => {
-        impl TIndex for $tp {
-            type MP = Vec<usize>;
-            $(RepeatFn!($op_fn, $fn_name($($arg_nm : $atp),*) -> $rt);)*
+        impl<T> Deref for $st_nm<T> {
+            type Target = T;
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
         }
     };
-
-    ($N:ident, $tp:ty, [$([$op_fn:ident, $fn_name:ident($($arg_nm:ident : $atp:ty),*) -> $rt:ty]),*]) => {
-        impl<const $N:usize> TIndex for $tp {
-            type MP = [usize; N];
-            $(RepeatFn!($op_fn, $fn_name($($arg_nm : $atp),*) -> $rt);)*
+    ($st_nm:ident($($tps:ty),*)) => {
+        {
+            struct $st_nm(($($tps),*));
+            impl Deref for $st_nm {
+                type Target = ($($tps),*);
+                fn deref(&self) -> &Self::Target {
+                    &self.0
+                }
+            }
         }
-    };    
+    };
 }
 
+// This trait converts all the possible indexing types
+// [usize; N], Vec<usize>, &[usize], etc, into only a few
+// manageable representations, that does not check bounds when indexed
+// (usually these representations are raw pointers)
+pub trait TsIndex {
+    type IndexT: TileIndex;
+    fn convert(&self) -> Self::IndexT;
+    fn mark_slice(s: Self::IndexT) -> SliceMarker<Self::IndexT> {
+        SliceMarker(s)
+    }
+}
 
-deriveTIndex!(Vec<usize>, [
-    [tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(&Vec<usize>, [
-    [tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(&[usize], [
-    [tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(UnsafeIndex<Vec<usize>>, [
-    [in_bounds_tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(UnsafeIndex<&Vec<usize>>, [
-    [in_bounds_tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(UnsafeIndex<&[usize]>, [
-    [in_bounds_tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(N, [usize; N], [
-    [static_tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [static_offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(N, &[usize; N], [
-    [static_tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [static_offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(N, UnsafeIndex<[usize; N]>, [
-    [in_bounds_static_tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [static_offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-deriveTIndex!(N, UnsafeIndex<&[usize; N]>, [
-    [in_bounds_static_tile_cartesian, tile_cartesian(dims: &[usize], strides: &[usize], len: usize) -> Option<usize>],
-    [static_offset_ind, offset(offsets: &[usize], strides: &[usize]) -> Self::MP]]);
-
-
-// This code is an abomination
-// I want to only use one trait to define two behaviors for usize
-// If WorldTensor is indexed with usize, regular pointer arithmetic applies
-// but if WorldSlice is indexed with usize, some tiling operation should
-// occur. Since only WorldSlice calls offset, I implement an inner behavior
+// TileIndex converts the representations generated by TsIndex
+// into actual linear indices, depending on the type marker
+// for example, the implementation for SliceMarker<UniversalIndex<T>> is different
+// to UniversalIndex<T>
+pub trait TileIndex {
+    type Meta;
+    fn tile_cartesian(&self, meta: Self::Meta) -> Option<usize>;
+}
 
 #[derive(Clone, Copy)]
-pub struct RequireRetileWrapper<T>(T, *const usize, usize);
+struct UniversalIndex<'a, T> {
+    ptr: *const T,
+    len: usize,
+    marker: PhantomData<&'a T>
+}
+#[derive(Clone, Copy)]
+struct StaticUIndex<'a, T, const N: usize> {
+    ptr: *const T,
+    marker: PhantomData<&'a T>
+}
+// type signifying that the index is indexed within a tensor slice
+auto_deref!(SliceMarker);
 
-impl TIndex for RequireRetileWrapper<usize> {
-    type MP = RequireRetileWrapper<usize>;
-    fn offset(&self, _offsets: &[usize], _strides: &[usize]) -> Self::MP {
-        *self
-    }
-    fn tile_cartesian(&self, _dims: &[usize], world_strides: &[usize], len: usize) -> Option<usize> {
-        let slice_strides = unsafe {
-            std::slice::from_raw_parts(self.1, self.2)
-        };
-        retile_cart(self.0, slice_strides, world_strides, len)
+// regular indexing where the memory order is contiguous and linear
+pub struct IndexReg<'a> {
+    pub dims: *const usize,
+    pub strides: *const usize,
+    pub len: usize,
+    pub nelems: usize,
+    pub marker: PhantomData<&'a usize>
+}
+
+// indexing on slices where the memory order is not contiguous
+pub struct IndexSlice<'a> {
+    pub dims: *const usize,
+    pub s_strides: *const usize,
+    pub g_strides: *const usize,
+    pub len: usize,
+    pub nelems: usize,
+    pub marker: PhantomData<&'a usize>
+}
+
+// regular indexing where the memory order is contiguous and linear
+// but the size of the indices are static
+struct StaticIndexReg<'a, const N: usize> {
+    dims: *const usize,
+    strides: *const usize,
+    marker: PhantomData<&'a usize>
+}
+
+// indexing on slices where the memory order is not contiguous
+// the sizes of the meta variables are static
+struct StaticIndexSlice<'a, const N: usize> {
+    dims: *const usize,
+    s_strides: *const usize,
+    g_strides: *const usize,
+    marker: PhantomData<&'a usize>
+}
+
+impl<'a> TileIndex for UniversalIndex<'a, usize> {
+    type Meta = IndexReg<'a>;
+    fn tile_cartesian(&self, meta: IndexReg<'a>) -> Option<usize> {
+        unsafe { 
+            if meta.len < self.len {
+                return None;
+            }
+            let mut idx: usize = 0;
+            let offset = meta.len - self.len;
+            for i in 0..self.len {
+                let j = i + offset;
+                if *self.ptr.add(i) >= *meta.dims.add(j) {
+                    return None;
+                }
+                idx += *meta.strides.add(j) * *self.ptr.add(i);
+            }
+            Some(idx)
+         }
     }
 }
 
-impl TIndex for usize {
-    type MP = RequireRetileWrapper<usize>;
-    fn offset(&self, _offsets: &[usize], strides: &[usize]) -> Self::MP {
-        RequireRetileWrapper(*self, strides.as_ptr(), strides.len())
-    }
-    fn tile_cartesian(&self, _dims: &[usize], _strides: &[usize], len: usize) -> Option<usize> {
-        check_index(*self, len)
+impl<'a, const N: usize> TileIndex for StaticUIndex<'a, usize, N> {
+    type Meta = IndexReg<'a>;
+    fn tile_cartesian(&self, meta: IndexReg<'a>) -> Option<usize> {
+        unsafe {
+            // N equals 0 represents the case where a single usize is used
+            // to index into the tensor, which just represents linear
+            // indexing
+            if N == 0 {
+                let idx = *self.ptr;
+                if idx >= meta.nelems {
+                    return None;
+                }
+                return Some(idx);
+            } else { // otherwise, an multidimensional index is used
+                if meta.len < N {
+                    return None;
+                }
+                let mut idx: usize = 0;
+                let offset = meta.len - N;
+                for i in 0..N {
+                    let j = i + offset;
+                    if *self.ptr.add(i) >= *meta.dims.add(j) {
+                        return None;
+                    }
+                    idx += *meta.strides.add(j) * *self.ptr.add(i);
+                }
+                return Some(idx)
+            }
+         }
     }
 }
 
-impl TIndex for UnsafeIndex<usize> {
-    type MP = RequireRetileWrapper<UnsafeIndex<usize>>;
-    fn offset(&self, _offsets: &[usize], strides: &[usize]) -> Self::MP {
-        RequireRetileWrapper(UnsafeIndex(self.0), strides.as_ptr(), strides.len())
-    }
-    fn tile_cartesian(&self, _dims: &[usize], _strides: &[usize], _len: usize) -> Option<usize> {
-        Some(self.0)
+impl<'a> TileIndex for SliceMarker<UniversalIndex<'a, usize>> {
+    type Meta = IndexSlice<'a>;
+    fn tile_cartesian(&self, meta: IndexSlice<'a>) -> Option<usize> {
+        if meta.len < self.len {
+            return None;
+        }
+        let mut idx: usize = 0;
+        let offset = meta.len - self.len;
+        unsafe { 
+            for i in 0..self.len {
+                let j = i + offset;
+                if *self.ptr.add(i) >= *meta.dims.add(j) {
+                    return None;
+                }
+                idx += *meta.g_strides.add(j) * *self.ptr.add(i);
+            }
+            Some(idx)
+         }
     }
 }
 
-impl TIndex for RequireRetileWrapper<UnsafeIndex<usize>> {
-    type MP = RequireRetileWrapper<UnsafeIndex<usize>>;
-    fn offset(&self, _offsets: &[usize], strides: &[usize]) -> Self::MP {
-        RequireRetileWrapper(UnsafeIndex(*self.0), strides.as_ptr(), strides.len())
-    }
-    fn tile_cartesian(&self, _dims: &[usize], world_strides: &[usize], len: usize) -> Option<usize> {
-        let slice_strides = unsafe {
-            std::slice::from_raw_parts(self.1, self.2)
-        };
-        inbounds_retile_cart(*self.0, slice_strides, world_strides, len)
+impl<'a, const N: usize> TileIndex for SliceMarker<StaticUIndex<'a, usize, N>> {
+    type Meta = IndexSlice<'a>;
+    fn tile_cartesian(&self, meta: IndexSlice<'a>) -> Option<usize> {
+        unsafe {
+            // if N equals 0, this represents indexing with a usize, which
+            // means indexing into a non-continuous block of memory
+            // represented using a slice. This has performance penalities
+            // and is supposed to be used with iterators with slices,
+            // if used at all.
+            if N == 0 {
+                let mut idx = 0;
+                let mut ind = *self.ptr;
+                for i in 0..meta.len {
+                    let c = ind / *meta.s_strides.add(i) as usize;
+                    idx += c * *meta.g_strides.add(i);
+                    ind -= c * *meta.s_strides.add(i);
+                    if ind <= 0 {
+                        break;
+                    }
+                }
+                if idx >= meta.nelems {
+                    return None;
+                }
+                return Some(idx);
+            } else { // otherwise more efficient multi-dimensional indexing into slices
+                if meta.len < N {
+                    return None;
+                }
+                let mut idx: usize = 0;
+                let offset = meta.len - N;
+                for i in 0..N {
+                    let j = i + offset;
+                    if *self.ptr.add(i) >= *meta.dims.add(j) {
+                        return None;
+                    }
+                    idx += *meta.g_strides.add(j) * *self.ptr.add(i);
+                }
+                return Some(idx);
+            }
+         }
     }
 }
-    
+
+impl<'a> TsIndex for &'a Vec<usize> {
+    type IndexT = UniversalIndex<'a, usize>;
+    fn convert(&self) -> Self::IndexT {
+        UniversalIndex{ptr: self.as_ptr(), len: self.len(), marker: PhantomData}
+    }
+}
+
+impl<'a> TsIndex for &'a [usize] {
+    type IndexT = UniversalIndex<'a, usize>;
+    fn convert(&self) -> Self::IndexT {
+        UniversalIndex{ptr: self.as_ptr(), len: self.len(), marker: PhantomData}
+    }
+}
+
+impl<'a, const N: usize> TsIndex for &'a [usize; N] {
+    type IndexT = StaticUIndex<'a, usize, N>;
+    fn convert(&self) -> Self::IndexT {
+        StaticUIndex{ptr: self.as_ptr(), marker: PhantomData}
+    }
+}
+
+impl<'a> TsIndex for &'a usize {
+    type IndexT = StaticUIndex<'a, usize, 0>;
+    fn convert(&self) -> Self::IndexT {
+        StaticUIndex{ptr: *self as *const usize, marker: PhantomData}
+    }
+}
