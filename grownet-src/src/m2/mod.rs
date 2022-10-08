789aa287689ba0ed272e1cfeb77a0819c3f04f38
AL <allanlin666666@gmail.com>
2022-10-07 20:17:44 -0400

    Preliminary Architecture

--- grownet-src/src/m2/mod.rs
+++ grownet-src/src/m2/mod.rs
@@ -1,4 +1,5 @@
-use std::{marker::PhantomData, ops::AddAssign};
+use std::mem;
+use std::ptr;
 use ndarray::prelude::*;
 use ndarray_rand::{RandomExt, rand_distr::Normal, rand_distr::Uniform};
 use num::{self, Float};
@@ -13,6 +14,10 @@ impl WeightedSigmoid {
     pub fn forward(&self, x: f32) -> f32 {
         1.0 / (1.0 + (self.b * (x - self.s)).exp())
     }
+
+    pub fn is_underflow(&self, x: f32, eps: f32) -> bool {
+        self.s - eps / self.b > x
+    }
 }
 struct Relu {}
 
@@ -22,9 +27,21 @@ impl Relu {
     }
 }
 
+#[derive(Clone, Copy)]
+struct Similarity {
+    ro: f32
+}
+
+impl Similarity {
+    fn similarity(a: &Similarity, b: &Similarity, strictness: f32) -> f32 {
+        strictness * ((a.ro - b.ro).cos() - 1.0).exp()
+    }
+}
+
 struct Message {
     msg: Array1<f32>,
-    mag: f32
+    mag: f32,
+    sim: Similarity
 }
 
 struct ComputeInstance {
@@ -41,14 +58,14 @@ impl ComputeInstance {
             act_fn: Relu{}
         }
     }
-    pub fn forward(&self, msg: Message) -> Message {
+    pub fn forward(&self, msg: &Message) -> Message {
         // y = W*x + b
         let mut k: Array<f32, Dim<[usize; 1]>> = self.w.dot(&msg.msg);
         k += &self.b;
         // yhat = relu(y)
         k.mapv_inplace(|x| self.act_fn.forward(x));
-        let norm = k.fold(0.0, |acc, x| acc + x * x).sqrt();
-        Message { msg: k, mag: norm }
+        let norm = l2_norm(&k);
+        Message { msg: k, mag: norm, sim: msg.sim }
     }
 }
 
@@ -83,16 +100,83 @@ impl RunningStats {
     }
 }
 
-struct ComputeNode {
-    act_fn: WeightedSigmoid,
-    ro: f32,
-    msg_accum: Message,
-    stats: RunningStats,
-    compute: Box<ComputeInstance>
+enum NodeResult {
+    NoResult,
+    Msg(Message),
 }
 
-impl ComputeNode {
-    pub fn receive(&self, msg: &Message) {
 
+struct ComputeNode {
+    act_fn: WeightedSigmoid,
+    sim: Similarity,
+    msg_accum: Message,
+    compute: *mut ComputeInstance,
+    compute_dim: usize,
+    compute_initialized: bool,
+}
+
+struct GlobalParams {
+    sim_strictness: f32,
+    underflow_epsilon: f32
+}
+
+struct GlobalCoordinates([usize; 2]);
+
+impl ComputeNode {
+    pub fn forward(&mut self, msg: &Message, global_params: GlobalParams) -> NodeResult {
+        let similarity = Similarity::similarity(&self.sim, &msg.sim, global_params.sim_strictness);
+        self.msg_accum.mag += msg.mag * similarity;
+        self.msg_accum.msg += &(&msg.msg * similarity);
+        if !self.act_fn.is_underflow(self.msg_accum.mag, global_params.underflow_epsilon) {
+            let output_msg;
+            unsafe {
+                if !self.compute_initialized {
+                    let compute = ComputeInstance::new(self.compute_dim);
+                    let mut compute = mem::ManuallyDrop::new(compute);
+                    self.compute = &mut (*compute) as *mut ComputeInstance;
+                    self.compute_initialized = true;
+                }
+                output_msg = (*self.compute).forward(&self.msg_accum);    
+            }
+            self.msg_accum.mag = 0.0;
+            self.msg_accum.msg *= 0.0;
+            NodeResult::Msg(output_msg)
+        } else {
+            NodeResult::NoResult
+        }
+    }
+
+    pub fn backward() {} 
+
+    pub fn forward_nodes() {}
+
+    pub fn backward_nodes() {}
+}
+
+impl Drop for ComputeNode {
+    fn drop(&mut self) {
+        unsafe {
+            if self.compute_initialized {
+                ptr::drop_in_place(self.compute);
+            }
+        }
+    }
+}
+
+fn l2_norm<T: Float>(a: &Array1<T>) -> T {
+    a.fold(T::zero(), |acc, x| acc + *x * *x).sqrt()
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn underflow() {
+        for i in 0..88 {
+            let x = i as f32;
+            let y = 1.0 / (1.0 + x.exp());
+            if !(y > 0.0) {
+                panic!("overflow at {i}");
+            }
+        }
     }
 }
--- grownet-src/src/main.rs
+++ grownet-src/src/main.rs
@@ -3,6 +3,7 @@
 mod m2;
 mod tensor;
 
+use std::marker::PhantomData;
 use std::process::Output;
 use std::fmt::Display;
 use ndarray_rand::rand_distr::uniform::SampleUniform;
@@ -12,8 +13,16 @@ use tensor as ts;
 use ndarray::prelude::*;
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::{Uniform, Normal, Distribution};
-
-
+use std::ptr;
+use std::mem;
 
 fn main() {
+    let k: *mut Vec<usize>;
+    unsafe {
+        let v = vec![1usize, 2, 3];
+        let mut v = mem::ManuallyDrop::new(v);
+        k = &mut *v as *mut Vec<usize>;
+        println!("val {}", (*k)[0]);
+        ptr::drop_in_place(k);
+    }
 }
