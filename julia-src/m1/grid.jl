aae39d410cbd7953d698438809d3e52f12b83bbc
AL <allanlin666666@gmail.com>
2022-11-05 21:30:17 -0400

    Better compute nodes

--- julia-src/m1/grid.jl
+++ julia-src/m1/grid.jl
@@ -1,62 +1,41 @@
-include("compute.jl")
-import Base.similar
 
-mutable struct BaselineGrid2D{
-    N<:AbstractNode, Msg, GradMsg, Ctx<:AbstractCtx} <: AbstractGrid
-    nodes::Array{N, 2}
+mutable struct BaselineGrid2D{Node, Ctx}
+    nodes::Array{Node, 2}
     node_ctx::Array{Ctx, 2}
-    sbuf::Array{Msg, 1}
-    rbuf::Array{Msg, 1}
     neighbour_offsets::NTuple{3, Int64}
 end
 
-function BaselineGrid2D(dims::NTuple{2, Int}, ::Type{Node}) where {Node <: AbstractNode}
+function BaselineGrid2D(dims::NTuple{2, Int}, ::Type{Node}) where {Node}
     offsets = (-1, 0, 1)
     nodes = Array{Node, 2}(undef, dims)
-    node_ctx = Array{ctx_type(Node), 2}(undef, dims)
+    node_ctx = Array{ctx_type(Node), 2}(undef, (dims[1]+1, dims[2]))
 
-    sbuf = Array{msg_type(Node), 1}(undef, dims[1] + 2) # generic function to compute this
-    rbuf = Array{msg_type(Node), 1}(undef, dims[1] + 2) # generic function to compute this
-
-    BaselineGrid2D{Node, msg_type(Node), grad_type(Node), ctx_type(Node)}(
+    BaselineGrid2D{Node, ctx_type(Node)}(
         nodes, 
         node_ctx,
-        sbuf,
-        rbuf,
         offsets
     )
 end
 
-function populate_all!(a::Array{S}, init_fn) where {S}
-    for i in CartesianIndices(a)
-        a[i] = init_fn(i)
-    end
-end
 
-function populate_all!(grid::BaselineGrid2D, node_init, ctx_init)
-    populate_all!(grid.nodes, node_init)
-    populate_all!(grid.node_ctx, ctx_init)
-end
-
-
-function forward(grid::BaselineGrid2D{Node}, msg::Vector) where {Node <: AbstractNode}
+function forward(grid::BaselineGrid2D{Node}, msg) where {Node}
     init_ctx!(grid.node_ctx, msg)
-    init_buf!(grid.sbuf, msg)
-    init_buf!(grid.rbuf, msg)
 end
 
 # should allocate no memory
-function forward_kernel(grid::BaselineGrid2D{Node}, msg::Vector) where {Node <: AbstractNode}
+function forward_kernel(grid::BaselineGrid2D{Node}, msg) where {Node}
     (d, l) = size(grid.nodes)
     for i in 1:d
-        grid.sbuf[i+1] = msg[i]
+        activate!(grid.node_ctx[i], msg[i])
     end
 
     for j in 1:l
         for i in 1:d
-            forward(grid.nodes[i, j], grid.node_ctx[i, j], grid.sbuf[i+1])
+            y = forward(grid.nodes[i, j], grid.node_ctx[i, j])
+            for k in grid.neighbour_offsets
+                activate!(grid.node_ctx[i + k, j+1], y)
+            end
         end
-        pool!(grid.sbuf, grid.rbuf)
-        grid.rbuf, grid.sbuf = grid.sbuf, grid.rbuf
     end
+    @view(grid.node_ctx[:, l])
 end
--- julia-src/m1/node.jl
+++ julia-src/m1/node.jl
@@ -1,30 +1,116 @@
-include("compute.jl")
-
-
 struct Relu end
 
-function forward(::Relu, x)
+function forward(::Type{Relu}, x)
     max(x, 0)
 end
 
-function backward(::Relu, g, x)
+function backward(::Type{Relu}, g, x)
     x > 0 ? 1 : 0 * g
 end
 
-struct LinearNode{T<:AbstractFloat, ActFn} <: AbstractNode
+function normalize!(a::T, mu_v, sd_v) where {T<:AbstractMatrix}
+    (d, l) = size(a)
+    @inbounds @simd for i in 1:l
+        mu = sum(@view(a[:, i])) / d
+        sd = zero(typeof(mu))
+        for j in 1:d
+            sd += (a[j, i] - mu) ^ 2
+        end
+        sd = sqrt(sd / (d - 1))
+        a[:, i] .= (@view(a[:, i]) .- mu) ./ (sd + 1e-6)
+        mu_v[i] = mu
+        sd_v[i] = sd
+    end
+end
+
+function d_normalize!(grad, x, dl_dx, mu_v, sd_v)
+    (d, l) = size(a)
+    for i in 1:l
+        mu = mu_v[i]
+        sd = sd_v[i]
+
+        grad_sum = 0
+        grad_dot = 0
+        for j in 1:d
+            grad_sum += grad[j, i]
+            grad_dot += grad[j, i] * x[j, i]
+        end
+        rem1 = 1 / (sd + 1e-6)
+        rem2 = 2 * mu / (d - 1) * sd
+        grad_sum /= d
+        grad_dot *= rem1
+
+        for j in 1:d
+            dl_dx[j, i] = rem1 * (grad[j, i] - grad_sum + grad_dot * (rem2 - (x[j, i] - mu)) / (sd * (d - 1)))
+        end
+    end
+end
+
+struct LinearNode{T<:AbstractFloat, ActFn}
     w::Matrix{T}
     b::Vector{T}
     act::ActFn
 end
 
-mutable struct LinearNodeCtx{T<:AbstractFloat, ActFn} <: AbstractCtx
+mutable struct LinearNodeCtx{T<:AbstractFloat, ActFn}
     x::Matrix{T}
     y::Matrix{T}
+    z::Matrix{T}
+    mu::Vector{T}
+    sd::Vector{T}
+
+    dl_dy::Matrix{T}
+    dy_dn::Matrix{T}
     dw::Matrix{T}
     db::Vector{T}
-    act::ActFn
 end
 
+mutable struct GeneralCtx{T<:AbstractFloat, N}
+    arr::Vector{T}
+    reserved::Int64
+    shapes::Vector{NTuple{N, Int64}}
+    offsets::Vector{Tuple{Int64, Int64}}
+end
+
+function GeneralCtx(::Type{T}, ::Val{N}) where {T<:AbstractFloat, N}
+    GeneralCtx{T, N}(
+        zeros(T, 1),
+        1,
+        Vector{NTuple{N, Int64}}(undef, 0),
+        Vector{Tuple{Int64, Int64}}(undef, 0)
+    )
+end
+
+struct CtxView
+    i::Int64
+end
+
+function reserve!(ctx::GeneralCtx{<:AbstractFloat, N}, shape::NTuple{N, Int64}) where {N}
+    alloc = prod(shape)
+    if ctx.reserved + alloc > length(ctx.arr)
+        resize!(ctx.arr, alloc + ctx.reserved)
+    end
+    ctx.arr[ctx.reserved:ctx.reserved + alloc] .= 0
+    push!(ctx.offsets, (ctx.reserved, alloc))
+    push!(ctx.shapes, shape)
+    ctx.reserved += alloc
+    CtxView(length(ctx.offsets))
+end
+
+function get_view(ctx::GeneralCtx, view_id::CtxView)
+    i = view_id.i
+    shape = ctx.shapes[i]
+    (offsets, size) = ctx.offsets[i]
+    slice = @view(ctx.arr[offsets:offsets + size-1])
+    slice = reshape(slice, shape)
+end
+    
+function clear!(ctx::GeneralCtx)
+    ctx.reserved = 1
+    empty!(ctx.offsets)
+    empty!(ctx.shapes)
+    nothing
+end
 
 function LinearNode(::Type{T}, ::Type{ActFn}, dim::Int64) where {T<:AbstractFloat, ActFn}
     LinearNode{T, ActFn}(
@@ -36,30 +122,33 @@ end
 
 function LinearNodeCtx(::Type{T}, ::Type{ActFn}, dim::Int64) where {T<:AbstractFloat, ActFn}
     LinearNodeCtx{T, ActFn}(
-        zeros(1,1),
-        zeros(1,1),
+        zeros(dim,1),
+        zeros(dim,1),
+        zeros(dim,1),
+        zeros(1),
+        zeros(1),
         zeros(dim, dim),
         zeros(dim),
         ActFn()
     )
 end
 
-ctx_type(::Type{ LinearNode{T, Act}}) where {T<:AbstractFloat, Act} = LinearNodeCtx{T, Act}
+ctx_type(::Type{LinearNode{T, Act}}) where {T<:AbstractFloat, Act} = LinearNodeCtx{T, Act}
 
-function forward(node::LinearNode, ctx::LinearNodeCtx, msg)
-    ctx.x = msg.x
-    ctx.y = node.w * msg.x .+ node.b
-    ctx.y ./= sqrt(size(node.w)[1])
-    out = similar(ctx.y)
-    for i in eachindex(out)
-        out[i] = forward(node.act, ctx.y[i])
+function forward(node::LinearNode, ctx::LinearNodeCtx)
+    ctx.y .= node.w * ctx.x .+ node.b
+    for i in eachindex(ctx.y)
+        ctx.y[i] = forward(node.ActFn, ctx.y[i])
     end
-    LinearNodeMsg(out)
+    ctx.z .= ctx.y
+    normalize!(ctx.z, ctx.mu, ctx.sd)
+    ctx.z
 end
 
-function backward(node::LinearNode, ctx::LinearNodeCtx, grad::LinearNodeMsg)
-    grad = backward.(node.act, grad, ctx.y)
-    ctx.db .+= sum(grad, dims = 2)
-    ctx.dw .+= grad * transpose(node.x)
-    transpose(node.w) * grad
+function backward(node::LinearNode, ctx::LinearNodeCtx)
+    d_normalize!(ctx.dl_dy, ctx.y, ctx.dy_dn, ctx.mu, ctx.sd)
+    ctx.dy_dn .= backward.(node.ActFn, ctx.dy_dn, ctx.y)
+    ctx.db .+= sum(ctx.dy_dn, dims = 2)
+    ctx.dw .+= ctx.dy_dn * transpose(ctx.x)
+    transpose(node.w) * ctx.dy_dn
 end
