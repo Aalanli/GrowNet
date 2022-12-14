49fc93351fcc5955d699f8fbd617c6c5163d082c
AL <allanlin666666@gmail.com>
2022-12-14 16:23:15 -0500

    GLMakie test

--- kernels/main.jl
+++ kernels/main.jl
@@ -1,7 +1,20 @@
-struct H
-    a::Vector{Float32}
-end
+using GLMakie
+GLMakie.activate!()
 
-struct C
-    s::
-end
+scene = Scene(backgroundcolor=:white)
+cam3d!(scene)
+
+# One can set the camera lookat and eyeposition, by getting the camera controls and using `update_cam!`
+camc = cameracontrols(scene)
+update_cam!(scene, camc, Vec3f(0, 8, 0), Vec3f(4.0, 0, 0))
+
+
+t = 0:0.1:15
+u = -1:0.1:1
+x = [u * sin(t) for t in t, u in u]
+y = [u * cos(t) for t in t, u in u]
+z = [t / 4 for t in t, u in u]
+surface!(scene, x, y, z; colormap = [:orangered, :orangered],
+    lightposition = Vec3f(0, 0, 0), ambient = Vec3f(0.65, 0.65, 0.65),
+    backlight = 5.0f0, figure = (; resolution = (1200, 800)))
+wireframe!(scene, x, y, z, overdraw = true, linewidth = 0.1) # try overdraw = true
