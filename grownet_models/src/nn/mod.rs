0f937d5d09be7cb046432e625b1e599a898f7efd
AL <allanlin666666@gmail.com>
2023-03-20 10:19:08 -0400

    weight tracer

--- grownet_models/src/nn/mod.rs
+++ grownet_models/src/nn/mod.rs
@@ -3,3 +3,52 @@ pub mod nd_ops;
 pub mod parts;
 
 pub use af_ops::Param;
+
+use std::any::{Any, TypeId};
+
+struct Test {
+    a: i32,
+    b: u32,
+}
+
+trait Trace: Sized + 'static {
+    fn trace<T: 'static, F: FnMut(&T)>(&self, mut f: F) {
+        let s: &dyn Any = &*self;
+        if let Some(v) = s.downcast_ref::<T>() {
+            f(v);
+        }
+    }
+    fn trace_mut<T: 'static, F: FnMut(&mut T)>(&mut self, mut f: F) {
+        let s: &mut dyn Any = &mut *self;
+        if let Some(v) = s.downcast_mut::<T>() {
+            f(v);
+        }
+    }
+}
+
+trait TracePath: 'static + Sized {
+    fn trace_path<T: 'static, F: FnMut(&str, &T)>(&self, path: &str, mut f: F) {
+        let s: &dyn Any = &*self;
+        if let Some(v) = s.downcast_ref::<T>() {
+            f(path, v);
+        }
+    }
+    fn trace_path_mut<T: 'static, F: FnMut(&str, &mut T)>(&mut self, path: &str, mut f: F) {
+        let s: &mut dyn Any = &mut *self;
+        if let Some(v) = s.downcast_mut::<T>() {
+            f(path, v);
+        }
+    }
+}
+
+impl Trace for Test {}
+
+
+#[test]
+fn test() {
+    let a = Test { a: 1, b: 2 };
+    a.trace(|_x: &Test| {
+
+    });
+}
+
